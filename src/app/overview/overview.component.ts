d3834a0a977974aa7d56baf04e20ec385e71a68e
Auriga <area51alienware@gmail.com>
2018-08-29 08:02:52 +0000

    Added side stock portfolio

--- src/app/overview/overview.component.ts
+++ src/app/overview/overview.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
 import $ from 'jquery';
 
 @Component({
@@ -7,7 +7,21 @@ import $ from 'jquery';
   styleUrls: ['./overview.component.css']
 })
 export class OverviewComponent implements OnInit {
-    public lineChartData:Array<any> = [
+  
+  public innerWidth: any;
+  public innerHeight: any;
+  
+  mystocks = [
+  { id: 1, name: 'Google', symbol: "GOOGL", shares: 5 },
+  { id: 2, name: 'Tesla', symbol: "TSLA", shares: 5 },
+  { id: 3, name: 'Apple', symbol: "AAPL", shares: 5 },
+  { id: 4, name: 'Microsoft', symbol: "MSFT", shares: 5 },
+  { id: 5, name: 'Micron', symbol: "MU", shares: 5 },
+  { id: 6, name: 'AMD', symbol: "AMD", shares: 5 },
+  { id: 7, name: 'Intel', symbol: "INTC", shares: 5 },
+];
+
+  public lineChartData:Array<any> = [
     {data: [65, 59, 80, 81, 56, 55, 40], label: ''},
   ];
   public lineChartLabels:Array<any> = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
@@ -33,7 +47,14 @@ export class OverviewComponent implements OnInit {
   }
 
   ngOnInit() {
-
+    this.innerWidth = window.innerWidth;
+    this.innerHeight = window.innerHeight; 
   }
-
+  /*
+  @HostListener('window:resize', ['$event'])
+  onResize(event) {
+    this.innerWidth = window.innerWidth;
+    this.innerHeight = window.innerHeight;
+  }
+  */
 }
